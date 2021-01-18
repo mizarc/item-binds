a144d80701d9164cbc77dde826e4bbe6e2c56a76
Mizarc <mizarc@protonmail.com>
2021-01-18 21:28:27 +1000

    Add method to make config with a specified name
    This method should take the argument and create a yaml file of that
    name. These files will serve as individual persistent items that can be
    called on individually.

--- src/main/java/xyz/mizarc/persistentitems/ItemConfigIO.java
+++ src/main/java/xyz/mizarc/persistentitems/ItemConfigIO.java
@@ -1,20 +1,32 @@
 package xyz.mizarc.persistentitems;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ItemConfigIO {
     private Plugin plugin;
     private File folder;
+    private File config;
 
     public ItemConfigIO(Plugin plugin) {
-        File folder = new File(plugin.getDataFolder(), "items");
+        folder = new File(plugin.getDataFolder(), "items");
         folder.mkdir();
     }
 
-    public void addItem() {
+    public void addItem(String id) {
+        File file = new File(folder, id + ".yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
 
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeItem() {
--- src/main/java/xyz/mizarc/persistentitems/PersistentItems.java
+++ src/main/java/xyz/mizarc/persistentitems/PersistentItems.java
@@ -1,10 +1,12 @@
 package xyz.mizarc.persistentitems;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.mizarc.persistentitems.commands.AddItemCommand;
 
 public class PersistentItems extends JavaPlugin {
-    public ItemConfigIO itemConfig;
+    private FileConfiguration config = getConfig();
+    private ItemConfigIO itemConfig;
 
     @Override
     public void onEnable() {
@@ -18,5 +20,9 @@ public class PersistentItems extends JavaPlugin {
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    public ItemConfigIO getItemConfig() {
+        return itemConfig;
     }
 }
