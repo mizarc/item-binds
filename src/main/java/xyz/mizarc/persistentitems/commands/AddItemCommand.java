b678379f778917f5a12cabbf51bc87baeb1f52ce
Mizarc <mizarc@protonmail.com>
2021-01-18 22:50:39 +1000

    Fix AddItem argument check and test config
    Can't check first index in argument if no arguments have been specified,
    so length is check instead.
    
    The item config add item is now being tested within the usages of a
    command.

--- src/main/java/xyz/mizarc/persistentitems/commands/AddItemCommand.java
+++ src/main/java/xyz/mizarc/persistentitems/commands/AddItemCommand.java
@@ -5,8 +5,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import xyz.mizarc.persistentitems.PersistentItems;
 
 public class AddItemCommand implements CommandExecutor {
+    private PersistentItems plugin;
+
+    public AddItemCommand(PersistentItems plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -15,13 +22,15 @@ public class AddItemCommand implements CommandExecutor {
             return false;
         }
 
-        if (args[0] == null) {
-            sender.sendMessage("Invalid arguments");
+        if (args.length == 0) {
+            sender.sendMessage("No arguments specified");
+            return false;
         }
 
         Player player = (Player) sender;
         ItemStack heldItem = player.getInventory().getItemInMainHand();
+        plugin.getItemConfig().addItem(args[0]);
 
-        return false;
+        return true;
     }
 }
