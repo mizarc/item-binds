79966b81b13e6fd2f71ec6cf5c99023e750c3aee
Mizarc <mizarc@protonmail.com>
2021-01-19 15:45:21 +1000

    Add methods to get items from config
    To allow getting all or specific items from the config, getters for
    getting one, all, or only active has been added. These generate Item
    objects that can be saved in memory.

--- src/main/java/xyz/mizarc/persistentitems/ItemConfigIO.java
+++ src/main/java/xyz/mizarc/persistentitems/ItemConfigIO.java
@@ -1,12 +1,16 @@
 package xyz.mizarc.persistentitems;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ItemConfigIO {
     private Plugin plugin;
@@ -14,8 +18,49 @@ public class ItemConfigIO {
     private File config;
 
     public ItemConfigIO(Plugin plugin) {
+        this.plugin = plugin;
         folder = new File(plugin.getDataFolder(), "items");
         folder.mkdir();
+    }
+
+    public Set<Item> getActiveItems() {
+        Set<Item> activeItems = new HashSet<>();
+        String[] fileNames = folder.list();
+        for (String fileName : fileNames) {
+            Item item = getItemIfActive(fileName);
+            activeItems.add(item);
+        }
+        return activeItems;
+    }
+
+    public Item getItem(String id) {
+        File file = new File(folder, id + ".yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+        ItemStack itemStack = new ItemStack(Material.getMaterial(config.getString("id")));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(config.getString("name"));
+        itemMeta.setLore(config.getStringList("lore"));
+        itemStack.setItemMeta(itemMeta);
+
+        return new Item(itemStack, config.getInt("slot"), config.getStringList("commands"));
+    }
+
+    public Item getItemIfActive(String id) {
+        File file = new File(folder, id + ".yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+        if (!config.getBoolean("active")) {
+            return null;
+        }
+
+        ItemStack itemStack = new ItemStack(Material.getMaterial(config.getString("id")));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(config.getString("name"));
+        itemMeta.setLore(config.getStringList("lore"));
+        itemStack.setItemMeta(itemMeta);
+
+        return new Item(itemStack, config.getInt("slot"), config.getStringList("commands"));
     }
 
     public void addItem(String id, ItemStack item, Integer slot) {
