ad533a2aab5dd2de7324adb1016e2a421a7c9098
Mizarc <mizarc@protonmail.com>
2021-01-19 13:00:47 +1000

    Add database connection and new table
    Database connects to an SQLite database labelled "data.db" and proceeds
    to create a table to hold all the players that are opting out of a
    persistent item's activation. This will ensure that the item is removed
    and will not come back until re-enabled.

--- src/main/java/xyz/mizarc/persistentitems/DatabaseConnection.java
+++ src/main/java/xyz/mizarc/persistentitems/DatabaseConnection.java
@@ -1,4 +1,49 @@
 package xyz.mizarc.persistentitems;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private PersistentItems plugin;
+    private Connection connection = null;
+
+    public DatabaseConnection(PersistentItems plugin) {
+        this.plugin = plugin;
+        openConnection();
+        createTable();
+    }
+
+    public void openConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + plugin.getDataFolder() + "/data.db");
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+    }
+
+    private void createTable() {
+        // Create table if it doesn't exit
+        String sqlCreate = "CREATE TABLE IF NOT EXISTS active_items (player TEXT NOT NULL, item TEXT NOT NULL, " +
+                "world TEXT NOT NULL);";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sqlCreate);
+            statement.executeUpdate();
+            statement.close();
+
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+
+    }
 }
