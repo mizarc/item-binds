394963dd29ca6a1038460f1fc162d254e12910d4
Mizarc <mizarc@protonmail.com>
2021-01-19 19:46:45 +1000

    Add method to give items on activation
    On the activate method being called, all players on the server are then
    given the item immediately. If it can't place the item in the slot as
    specified, it attempts to add it in the first available slot.

--- src/main/java/xyz/mizarc/persistentitems/commands/ActivateCommand.java
+++ src/main/java/xyz/mizarc/persistentitems/commands/ActivateCommand.java
@@ -1,16 +1,24 @@
 package xyz.mizarc.persistentitems.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import xyz.mizarc.persistentitems.Item;
 import xyz.mizarc.persistentitems.ItemConfigIO;
+import xyz.mizarc.persistentitems.ItemContainer;
 import xyz.mizarc.persistentitems.PersistentItems;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivateCommand implements CommandExecutor {
     PersistentItems plugin;
 
-    ActivateCommand(PersistentItems plugin) {
+    public ActivateCommand(PersistentItems plugin) {
         this.plugin = plugin;
     }
 
@@ -26,9 +34,23 @@ public class ActivateCommand implements CommandExecutor {
             return true;
         }
 
+        ItemContainer itemContainer = plugin.getItemContainer();
         ItemConfigIO itemConfig = plugin.getItemConfig();
-        plugin.getItemContainer().loadItem(itemConfig.getItem(args[0]));
+        itemContainer.loadItem(itemConfig.getItem(args[0]));
         itemConfig.setActive(args[0], true);
+        giveActivatedItem(itemContainer.getItem(args[0]).getItemStack(plugin),
+                itemContainer.getItem(args[0]).getSlot());
         return true;
+    }
+
+    private void giveActivatedItem(ItemStack item, Integer slot) {
+        List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+        for (Player player : players) {
+            Inventory inventory = player.getInventory();
+            if (inventory.getItem(slot) == null) {
+                inventory.setItem(slot, item);
+            }
+            inventory.addItem(item);
+        }
     }
 }
