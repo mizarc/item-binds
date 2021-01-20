48e7b7013676ebbf987cfed3dbb90294e52a343b
Mizarc <mizarc@protonmail.com>
2021-01-20 14:50:30 +1000

    Add simple methods to query the database table
    Table has been slightly modified to use the name "hidden_items". From
    this, entries can be added, removed and queried based on the player
    UUID, item name, and world.

--- src/main/java/xyz/mizarc/persistentitems/DatabaseConnection.java
+++ src/main/java/xyz/mizarc/persistentitems/DatabaseConnection.java
@@ -1,9 +1,6 @@
 package xyz.mizarc.persistentitems;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseConnection {
     private PersistentItems plugin;
@@ -31,9 +28,62 @@ public class DatabaseConnection {
         }
     }
 
+    public boolean isHidden(String playerUUID, String itemID, String world) {
+        String query = "SELECT * FROM hidden_items WHERE player=? AND item=? AND world=?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, playerUUID);
+            statement.setString(2, itemID);
+            statement.setString(3, world);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (!resultSet.next()) {
+                return false;
+            }
+            return true;
+
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean addHidden(String playerUUID, String itemID, String world) {
+        String query = "INSERT INTO hidden_items (player, item, world) VALUES (?, ?, ?)";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, playerUUID);
+            statement.setString(2, itemID);
+            statement.setString(3, world);
+            statement.executeUpdate();
+            statement.close();
+
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+    }
+
+    public boolean removeHidden(String playerUUID, String itemID, String world) {
+        String query = "DELETE FROM hidden_items WHERE player=? AND item=? AND world=?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, playerUUID);
+            statement.setString(2, itemID);
+            statement.setString(3, world);
+            statement.executeUpdate();
+            statement.close();
+
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+    }
+
     private void createTable() {
         // Create table if it doesn't exit
-        String sqlCreate = "CREATE TABLE IF NOT EXISTS active_items (player TEXT NOT NULL, item TEXT NOT NULL, " +
+        String sqlCreate = "CREATE TABLE IF NOT EXISTS hidden_items (player TEXT NOT NULL, item TEXT NOT NULL, " +
                 "world TEXT NOT NULL);";
 
         try {
@@ -44,6 +94,5 @@ public class DatabaseConnection {
         } catch (SQLException error) {
             error.printStackTrace();
         }
-
     }
 }
